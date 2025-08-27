225dd757f8e7e5da9ddc85114e94ff427c7c0a6f
scorpion9ine <mauriciomiguel.edu@gmail.com>
2025-08-26 22:34:09 -0300

    first commit

---
+++ src/components/Headercomponent.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li class="atual">
+          <RouterLink to="/"> Home </RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/about">About</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/projects"> Projects </RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/connect"> Connect </RouterLink>
+        </li>
+
+      </ul>
+    </nav>
+  </header>
+
+</template>
+
+<style>
+  header {
+    padding: 5vw 10vw;
+    font-size: 0.8rem;
+
+    & ul {
+      display: flex;
+      width: fit-content;
+      margin: 0 auto;
+      background: rgba(255, 255, 255, 0.5);
+      padding: 5px;
+      border-radius: 50px;
+      border: 1px solid rgb(211, 211, 211);
+
+      & li {
+        padding: 8px 15px;
+      }
+
+      & .router-link-active {
+        background: black;
+        color: white;
+        border-radius: 30px;
+        padding: 5px 5px;
+      }
+    }
+  }
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Headercomponent from '../components/Headercomponent.vue';
+import FooterComponent from '../components/FooterComponent.vue';   
+
+</script>
+
+<template>
+<Headercomponent></Headercomponent>
+ <main>
+        <section class="banner">
+
+            <h1> <span>About</span> Alex</h1>
+
+            <p>
+                With 5+ years in design, I've worked on diverse projects, from mobile apps to web platforms, focusing on user-centered designs that are both visually appealing and practical.
+            </p>
+        </section>
+    </main>
+<footer-component></footer-component>
+</template>
+
+<style>
+
+</style>
